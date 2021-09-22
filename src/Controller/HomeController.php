ad44334346aa2cad200650dae7b99e99a0787bb6
evan-azemard <evan-azemard@laplateforme.io>
2021-09-22 13:36:17 +0200

    add comment

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Post;
+use App\Form\CommentType;
 use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -29,10 +33,25 @@ class HomeController extends AbstractController
     /**
      * @Route ("/posts/{id}", name="show_post")
      */
-    public function show(Post $post){
+    public function show(Post $post, Request $request, EntityManagerInterface $em){
 
+        $comments = new Comment();
+
+       $form = $this->createForm(CommentType::class, $comments );
+       $comments->setPost($post);
+
+       $form->handleRequest($request);
+
+       if ($form->isSubmitted() && $form->isValid() ){
+
+           $em->persist($comments);
+           $em->flush();
+
+           var_dump($comments);
+       }
         return $this->render('home/post.html.twig', [
-            'post' => $post
+            'post' => $post,
+            "form" => $form->createView()
         ]);
 
     }
