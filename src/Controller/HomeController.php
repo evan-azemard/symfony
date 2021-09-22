41300739f5e17321d31c37009bf58baa73786b51
evan-azemard <evan-azemard@laplateforme.io>
2021-09-22 11:03:51 +0200

    cards, id_cards

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,38 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'home')]
-    public function index(): Response
+    /**
+     * @Route("/home", name="home")
+     */
+
+    public function index(PostRepository $ripo)
     {
+
+        $posts = $ripo->findAll();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'posts' => $posts
         ]);
+    }
+
+
+
+
+    /**
+     * @Route ("/posts/{id}", name="show_post")
+     */
+    public function show(Post $post){
+
+        return $this->render('home/post.html.twig', [
+            'post' => $post
+        ]);
+
     }
 }
